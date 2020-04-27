fd09271b80aba3ed2d7456eff2d2bf1a6292794d
rooiboss00 <rooiboss@yahoo.co.jp>
2020-04-27 04:11:04 +0000

    Complete advanced setup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get root_url
     assert_response :success
+  end
   
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
